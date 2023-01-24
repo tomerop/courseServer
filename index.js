bb8121e7b97dbe352fbb1235f5877d1c0dba3f25
tomerop <117067831+tomerop@users.noreply.github.com>
2023-01-24 15:24:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const authRouter = require('./routes/auth')
 const app = express();
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(*))
 app.use('/api/users',usersRouter);
 app.use('/api/auth', authRouter)
 app.use('/api/courses',coursesRouter);
